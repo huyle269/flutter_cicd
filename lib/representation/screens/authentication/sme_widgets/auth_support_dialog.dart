c91f33cce930def2c4bd9c6dbd94005951798b75
HuyLQ19 <quanghuy.le90@gmail.com>
2021-10-14 18:02:20 +0700

    Add support dialog

---
+++ lib/representation/screens/authentication/sme_widgets/auth_support_dialog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthSupportDialog extends StatefulWidget {
+  const AuthSupportDialog({Key? key}) : super(key: key);
+
+  @override
+  _AuthSupportDialogState createState() => _AuthSupportDialogState();
+}
+
+class _AuthSupportDialogState extends State<AuthSupportDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
